e3ffb74a88b040cb3f1335deefe4f5dda5680e28
giovane358 <giovanesilva.etec@gmail.com>
2019-05-08 20:15:35 -0300

    Exercio20

---
+++ src/main/java/br/com/etechoracio/exercio20/Exercio20Application.java
@@ -0,0 +1,13 @@
+package br.com.etechoracio.exercio20;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercio20Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercio20Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/etechoracio/exercio20/Exercio20ApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.etechoracio.exercio20;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Exercio20ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
